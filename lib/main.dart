3a5beb2ac6e17813f27f1c44928beaed4017e286
RUzai <RUzairov@gmail.com>
2023-09-16 16:21:19 +0300

    clear main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:avtonalivator/presentation/main_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,34 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Main(),
+      home: const SizedBox(),
     );
-  }
-}
-
-class Main extends StatelessWidget {
-  const Main({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider<MainCubit>(
-      create: (_) => MainCubit(),
-      child: BlocBuilder<MainCubit, MainState>(
-        builder: (context, state) {
-          if (state is MainStatus) {
-            if (!state.isAvailable) {
-              return const Text("Bluetooth is not available");
-            }
-            if (!state.isEnabled) {
-              return const Text("Bluetooth is not enabled");
-            } else {
-              return const Text("Select device");
-            }
-          }
-          return const SizedBox();
-        },
-      ),
-    );
-    return const SizedBox();
   }
 }
