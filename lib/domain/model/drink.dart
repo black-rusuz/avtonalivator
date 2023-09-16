95082ee841819e6867891bc55b0f60166c23c150
RUzai <RUzairov@gmail.com>
2023-09-17 01:11:55 +0300

    fix model id

--- lib/domain/model/drink.dart
+++ lib/domain/model/drink.dart
@@ -3,16 +3,22 @@ import 'package:equatable/equatable.dart';
 import '../../data/model/drink.dart';
 
 class UiDrink extends Equatable {
+  final int id;
   final String name;
   final int volume;
 
   const UiDrink({
+    required this.id,
     required this.name,
     required this.volume,
   });
 
   factory UiDrink.fromApi(ApiDrink drink) {
-    return UiDrink(name: drink.name, volume: drink.volume);
+    return UiDrink(
+      id: drink.hashCode,
+      name: drink.name,
+      volume: drink.volume,
+    );
   }
 
   @override
