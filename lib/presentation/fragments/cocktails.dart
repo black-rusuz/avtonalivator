cde798ff6b00bc8a4a99596b53f3260fd92a9dc6
RUzai <RUzairov@gmail.com>
2023-09-17 00:14:51 +0300

    cocktails

--- lib/presentation/fragments/cocktails.dart
+++ lib/presentation/fragments/cocktails.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/cupertino.dart';
 
-import '../strings.dart';
+import '../../domain/model/cocktail.dart';
+import '../widgets/cocktail_card.dart';
 
 class CocktailsFragment extends StatelessWidget {
   const CocktailsFragment({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(Strings.cocktails),
+    return ListView.separated(
+      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
+      itemCount: 8,
+      itemBuilder: itemBuilder,
+      separatorBuilder: separatorBuilder,
     );
+  }
+
+  Widget itemBuilder(BuildContext context, int index) {
+    const item = UiCocktail(id: 0, name: 'name', image: 'image', drinks: []);
+    return CocktailCard(
+      cocktail: item,
+      onItemTap: (v) {},
+    );
+  }
+
+  Widget separatorBuilder(BuildContext context, int index) {
+    return const SizedBox(height: 10);
   }
 }
---
+++ lib/presentation/widgets/cocktail_card.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../../core/theme.dart';
+import '../../domain/model/cocktail.dart';
+import 'basic_card.dart';
+
+class CocktailCard extends StatelessWidget {
+  final UiCocktail cocktail;
+  final ValueChanged<UiCocktail> onItemTap;
+
+  const CocktailCard({
+    super.key,
+    required this.cocktail,
+    required this.onItemTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const BasicCard(
+      padding: EdgeInsets.all(20),
+      color: AppTheme.white,
+      child: Text('Card'),
+    );
+  }
+}
