9a53e89e86336ae4a7a672a032134b0093ff32b3
RUzai <RUzairov@gmail.com>
2023-09-17 21:27:33 +0300

    CocktailsList

--- lib/presentation/fragments/cocktails/cocktails.dart
+++ lib/presentation/fragments/cocktails/cocktails.dart
@@ -1,23 +1,39 @@
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 
 import '../../../domain/model/cocktail.dart';
+import 'cocktails_provider.dart';
 import 'widgets/cocktail_card.dart';
+
+export 'cocktails_provider.dart';
 
 class CocktailsFragment extends StatelessWidget {
   const CocktailsFragment({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final cocktails = context.watch<CocktailsProvider>().cocktails;
+    return _CocktailsList(cocktails: cocktails);
+  }
+}
+
+class _CocktailsList extends StatelessWidget {
+  final List<UiCocktail> cocktails;
+
+  const _CocktailsList({required this.cocktails});
+
+  @override
+  Widget build(BuildContext context) {
     return ListView.separated(
       padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
-      itemCount: 8,
+      itemCount: cocktails.length,
       itemBuilder: itemBuilder,
       separatorBuilder: separatorBuilder,
     );
   }
 
   Widget itemBuilder(BuildContext context, int index) {
-    const item = UiCocktail(id: 0, name: 'name', image: 'image', drinks: []);
+    final item = cocktails[index];
     return CocktailCard(
       cocktail: item,
       onItemTap: (v) {},
