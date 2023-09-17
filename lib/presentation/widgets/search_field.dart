31188599b9cb413f065d2e9b4dc28198ef04d05b
RUzai <RUzairov@gmail.com>
2023-09-18 02:20:08 +0300

    SearchField label background

--- lib/presentation/widgets/search_field.dart
+++ lib/presentation/widgets/search_field.dart
@@ -30,8 +30,15 @@ class SearchField extends StatelessWidget {
           onChanged: onChanged,
           controller: controller,
           textCapitalization: TextCapitalization.sentences,
-          decoration: const InputDecoration(
-            labelText: Strings.enterName,
+          decoration: InputDecoration(
+            label: Container(
+              padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 4),
+              decoration: const BoxDecoration(
+                color: AppTheme.background,
+                borderRadius: AppTheme.borderRadius,
+              ),
+              child: const Text(Strings.enterName),
+            ),
           ),
         ),
       ),
