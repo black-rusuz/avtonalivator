6ef29426cc28fdfad8bcac499d1ecfcb6c0a6d48
RUzai <RUzairov@gmail.com>
2023-09-23 16:14:13 +0300

    connector class

--- lib/data/connector.dart
+++ lib/data/connector.dart
@@ -1,4 +1,47 @@
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:injectable/injectable.dart';
 
 @singleton
-class Connector {}
+class Connector {
+  final _bluetooth = FlutterBluetoothSerial.instance;
+
+  Connector();
+
+  BluetoothConnection? connection;
+
+  Future<bool> get isDiscovering async {
+    final value = await _bluetooth.isDiscovering ?? false;
+    return value;
+  }
+
+  Stream<BluetoothDevice> get devices async* {
+    await _bluetooth.cancelDiscovery();
+    final scanResultStream = _bluetooth.startDiscovery();
+
+    await for (final result in scanResultStream) {
+      yield result.device;
+    }
+  }
+
+  Future<BluetoothConnection?> connect(BluetoothDevice device) async {
+    try {
+      _connect(device.address);
+    } catch (_) {
+      // TODO: log
+    }
+    return connection;
+  }
+
+  Future<void> cancelDiscovery() {
+    return _bluetooth.cancelDiscovery();
+  }
+
+  Future<BluetoothConnection?> _connect(String address) async {
+    await connection?.close();
+
+    final attempt = await BluetoothConnection.toAddress(address);
+    if (attempt.isConnected) connection = attempt;
+
+    return connection;
+  }
+}
