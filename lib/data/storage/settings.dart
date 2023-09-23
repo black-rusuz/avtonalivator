fc5a010b7fa003ca893e0f6cadad50a3f03d673c
RUzai <RUzairov@gmail.com>
2023-09-23 16:29:24 +0300

    settings autoConnect

--- lib/data/storage/settings.dart
+++ lib/data/storage/settings.dart
@@ -1,6 +1,7 @@
 import 'package:hive/hive.dart';
 import 'package:injectable/injectable.dart';
 
+const _autoConnect = 'autoConnect';
 const _lastAddress = 'lastAddress';
 const _pumpsQuantity = 'pumpsQuantity';
 
@@ -9,21 +10,30 @@ class SettingsBox {
   static const name = 'settings';
   final _box = Hive.box(name);
 
-  String get lastAddress {
-    final value = _box.get(_lastAddress);
-    return value ?? '';
+  bool get autoConnect {
+    final value = _box.get(_autoConnect);
+    return value ?? false;
   }
 
-  set lastAddress(String value) {
-    _box.put(_lastAddress, value);
+  set autoConnect(bool value) {
+    _box.put(_autoConnect, value);
   }
 
-  String get pumpsQuantity {
+  // String get lastAddress {
+  //   final value = _box.get(_lastAddress);
+  //   return value ?? '';
+  // }
+  //
+  // set lastAddress(String value) {
+  //   _box.put(_lastAddress, value);
+  // }
+
+  int get pumpsQuantity {
     final value = _box.get(_pumpsQuantity);
-    return value ?? '';
+    return value ?? 6;
   }
 
-  set pumpsQuantity(String value) {
+  set pumpsQuantity(int value) {
     _box.put(_pumpsQuantity, value);
   }
 }
