d1e9625e4f804f3780595b43d3553ef5248eb72e
RUzai <RUzairov@gmail.com>
2023-09-23 15:37:07 +0300

    init boxes

--- lib/core/setup.dart
+++ lib/core/setup.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/cupertino.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
+import '../data/storage/settings.dart';
+import '../data/storage/stats.dart';
 import '../injection.dart';
 
 Future<void> setupApp() async {
   WidgetsFlutterBinding.ensureInitialized();
   initServices();
+}
+
+Future<void> initHive() async {
+  await Hive.initFlutter();
+  await Hive.openBox(SettingsBox.name);
+  await Hive.openBox(StatsBox.name);
 }
