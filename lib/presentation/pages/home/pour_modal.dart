5e9d3c7b29d03d3052196aa9af6635e1288a084d
RUzai <RUzairov@gmail.com>
2023-09-30 19:38:28 +0300

    PourModal percent value

--- lib/presentation/pages/home/pour_modal.dart
+++ lib/presentation/pages/home/pour_modal.dart
@@ -7,7 +7,6 @@ import 'package:google_fonts/google_fonts.dart';
 import '../../../core/theme.dart';
 import '../../provider/connection.dart';
 import '../../strings.dart';
-import '../../widgets/basic_card.dart';
 import '../../widgets/percent_indicator.dart';
 
 class PourModal extends StatelessWidget {
@@ -16,40 +15,44 @@ class PourModal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final provider = context.watch<ConnectionProvider>();
-    final value = provider.percent / 100;
     final drink = provider.drink;
+    final value = provider.percent / 100;
+    final percentString = provider.percent.toString() + '%';
 
     // TODO: read mode
     // final finish = mode == DeviceMode.wait;
     final finish = value >= 1 && drink == null;
     if (finish) Navigator.of(context).pop();
 
-    return BasicCard(
-      child: ListView(
-        padding: AppTheme.listPadding,
-        controller: PrimaryScrollController.of(context),
-        children: [
-          Text(
-            drink ?? Strings.loading,
-            textAlign: TextAlign.center,
-            style: GoogleFonts.inter(
-              color: AppTheme.black,
-              fontSize: 24,
-              fontWeight: FontWeight.w900,
+    return ListView(
+      shrinkWrap: true,
+      padding: AppTheme.listPadding,
+      controller: PrimaryScrollController.of(context),
+      children: [
+        Text(
+          drink ?? Strings.loading,
+          textAlign: TextAlign.center,
+          style: GoogleFonts.inter(
+            color: AppTheme.black,
+            fontSize: 24,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+        Padding(
+          padding: AppTheme.padding * 4,
+          child: PercentIndicator(
+            percent: min(value, 1),
+            animation: false,
+            child: Text(
+              percentString,
+              style: GoogleFonts.inter(
+                fontSize: 48,
+                fontWeight: FontWeight.w900,
+              ),
             ),
           ),
-          Padding(
-            padding: AppTheme.padding * 4,
-            child: PercentIndicator(
-              percent: min(value, 1),
-              animation: false,
-            ),
-          ),
-          LinearProgressIndicator(
-            value: min(value, 1),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
