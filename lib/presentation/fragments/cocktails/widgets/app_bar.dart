369e4cb02186fc10382abfc858bda4b2be76b18f
RUzai <RUzairov@gmail.com>
2023-12-01 22:44:08 +0300

    fix appbar

--- lib/presentation/fragments/cocktails/widgets/app_bar.dart
+++ lib/presentation/fragments/cocktails/widgets/app_bar.dart
@@ -10,37 +10,27 @@ class _CocktailsAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final query = MediaQuery.of(context);
     final height = query.size.height * 0.4;
-    // const height = kToolbarHeight + SearchField.height;
-    final topPadding = MediaQuery.of(context).viewPadding.top;
 
     return SliverAppBar(
       pinned: true,
       collapsedHeight: kToolbarHeight,
       expandedHeight: height,
       backgroundColor: AppTheme.background,
+      title: Container(
+        padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 12),
+        decoration: const BoxDecoration(
+          color: AppTheme.background,
+          borderRadius: AppTheme.borderRadius,
+        ),
+        child: Text(
+          Strings.cocktails,
+          style: AppTheme.pageTitle,
+        ),
+      ),
       flexibleSpace: FlexibleSpaceBar(
         background: Stack(
           children: [
             if (background != null) Positioned.fill(child: background!),
-            Positioned.fill(
-              /// Подобрал рандомно
-              top: topPadding + 12.5,
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 2, horizontal: 12),
-                  decoration: const BoxDecoration(
-                    color: AppTheme.background,
-                    borderRadius: AppTheme.borderRadius,
-                  ),
-                  child: Text(
-                    Strings.cocktails,
-                    style: AppTheme.pageTitle,
-                  ),
-                ),
-              ),
-            ),
           ],
         ),
       ),
