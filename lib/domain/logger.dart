051155c6422a874824836fbd83745aa3d7c35436
RUzai <RUzairov@gmail.com>
2023-12-01 23:07:54 +0300

    fix loggers

--- lib/domain/logger.dart
+++ lib/domain/logger.dart
@@ -5,20 +5,19 @@ class Logger {
   static Function(dynamic, [Object?, StackTrace?]) get log =>
       kDebugMode ? _logDebug : _logProd;
 
-  static void _logDebug(
-    dynamic message, [
-    Object? error,
-    StackTrace? stack,
-  ]) {
+  static void _logDebug(dynamic message, [Object? data, StackTrace? stack]) {
     if (kDebugMode) print(message);
-    if (stack != null || error != null) {
-      debugPrintStack(stackTrace: stack, label: error.toString());
+    if (stack != null) {
+      debugPrintStack(stackTrace: stack, label: data.toString());
     }
   }
 
-  static void _logProd(dynamic message, [Object? error, StackTrace? stack]) {
+  static void _logProd(dynamic message, [Object? data, StackTrace? stack]) {
+    String m = message.toString();
+    if (data != null) m += ':\n$data';
+
     final crashlytics = FirebaseCrashlytics.instance;
-    crashlytics.log(message);
-    if (stack != null || error != null) crashlytics.recordError(error, stack);
+    crashlytics.log(m);
+    if (stack != null) crashlytics.recordError(data, stack);
   }
 }
