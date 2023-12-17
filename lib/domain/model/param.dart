f86986aebf5638a3bc33f7033f3adb8884fbcd51
RUzai <RUzairov@gmail.com>
2023-12-17 15:41:44 +0300

    Param class

--- lib/domain/model/param.dart
+++ lib/domain/model/param.dart
@@ -1,19 +1,56 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
 
-class Param<T> extends Equatable {
+import '../../presentation/fragments/settings/provider.dart';
+
+class ParamKey {
+  static const autoConnect = 'autoConnect';
+  static const pumpsQuantity = 'pumpsQuantity';
+}
+
+class Param extends Equatable {
   final String key;
   final String title;
   final String? description;
-  final T value;
-  final List<T> options;
+  final dynamic value;
+  final Function onTap;
 
-  const Param({
+  const Param._({
     required this.key,
     required this.title,
     this.description,
-    required this.value,
-    this.options = const [],
+    this.value,
+    required this.onTap,
   });
+
+  factory Param.stored({
+    required SettingsProvider provider,
+    required String key,
+    required String title,
+    String? description,
+    required dynamic defaultValue,
+  }) {
+    return Param._(
+      key: key,
+      title: title,
+      value: provider.getParam(key, defaultValue),
+      onTap: (v) => provider.setParam(key, v),
+    );
+  }
+
+  factory Param.action({
+    required String title,
+    String? description,
+    required VoidCallback onTap,
+  }) {
+    final key = UniqueKey();
+    return Param._(
+      key: key.toString(),
+      title: title,
+      description: description,
+      onTap: onTap,
+    );
+  }
 
   @override
   List<Object?> get props => [
@@ -21,6 +58,6 @@ class Param<T> extends Equatable {
         title,
         description,
         value,
-        [...options],
+        onTap,
       ];
 }
