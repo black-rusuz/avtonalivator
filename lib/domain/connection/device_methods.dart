d2ab8f666b0942f8ca3c401e6ee4ed01d0e5a73d
RUzai <RUzairov@gmail.com>
2023-12-23 16:07:00 +0300

    DeviceMethods impl

--- lib/domain/connection/device_methods.dart
+++ lib/domain/connection/device_methods.dart
@@ -1,4 +1,8 @@
+import 'package:injectable/injectable.dart';
+
 import '../model/cocktail.dart';
+import '../model/device_data.dart';
+import 'connector.dart';
 
 abstract interface class DeviceMethods {
   Stream<DeviceData> get deviceData;
@@ -16,5 +20,50 @@ abstract interface class DeviceMethods {
   Future<void> setLightningMode(int value);
 }
 
-// @Injectable(as: deviceMethods)
-// class FbsDeviceMethods implements DeviceMethods {}
+@Injectable(as: DeviceMethods)
+class FbsDeviceMethods implements DeviceMethods {
+  final Connector _connector;
+
+  FbsDeviceMethods(this._connector);
+
+  @override
+  Stream<DeviceData> get deviceData {
+    return _connector.input;
+  }
+
+  @override
+  Future<void> setCocktail(UiCocktail cocktail) {
+    // TODO: implement startPour
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> startPour() {
+    // TODO: implement startPour
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> stopPour() {
+    // TODO: implement stopPour
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> calibrate(int weight) {
+    // TODO: implement calibrate
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> setLightningMode(int value) {
+    // TODO: implement setLightningMode
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> setLightningBrightness(int value) {
+    // TODO: implement setLightningBrightness
+    throw UnimplementedError();
+  }
+}
