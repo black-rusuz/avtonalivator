2622a7a1ed7f256d605eb33af05d6730a19df0d0
RUzai <RUzairov@gmail.com>
2023-12-23 15:38:46 +0300

    interface methods

--- lib/domain/connection/device_methods.dart
+++ lib/domain/connection/device_methods.dart
@@ -1,4 +1,20 @@
-abstract interface class DeviceMethods {}
+import '../model/cocktail.dart';
+
+abstract interface class DeviceMethods {
+  Stream<DeviceData> get deviceData;
+
+  Future<void> setCocktail(UiCocktail cocktail);
+
+  Future<void> startPour();
+
+  Future<void> stopPour();
+
+  Future<void> calibrate(int weight);
+
+  Future<void> setLightningBrightness(int value);
+
+  Future<void> setLightningMode(int value);
+}
 
 // @Injectable(as: deviceMethods)
 // class FbsDeviceMethods implements DeviceMethods {}
