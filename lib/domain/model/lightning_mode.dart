a85b796e7c643a2a91cc0a321dd0abec30245e10
RUzai <RUzairov@gmail.com>
2023-12-24 00:28:51 +0300

    LightingMode

---
+++ lib/domain/model/lightning_mode.dart
@@ -0,0 +1,26 @@
+enum LightingMode {
+  mode1(1),
+  mode2(2),
+  mode3(3),
+  mode4(4),
+  mode5(5),
+  mode6(6),
+  mode7(7),
+  mode8(8),
+  mode9(9);
+
+  // TODO: LightingMode char
+  static const char = '';
+  final int value;
+
+  const LightingMode(this.value);
+
+  factory LightingMode.fromNumber(int number) {
+    return values.firstWhere(
+      (mode) => mode.value == number,
+      orElse: () => LightingMode.mode1,
+    );
+  }
+
+  String get command => 'm$value';
+}
